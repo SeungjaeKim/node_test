7f0541bfcf51337749046ae91102ddd4208033f0
SeungjaeKim <kimseungjae@gmail.com>
2021-05-13 22:58:51 +0900

    패스트 캠퍼스 자바스크립트 공부중...

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("hello node");
+import getType from './getType'
+
+console.log(typeof "hello js")
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof {})
+console.log(typeof [])
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType(null))
+console.log(getType({}))
+console.log(getType([]))
+
+// 산술 연산자(arithmetic operator)
+console.log(1 + 2)
+console.log(5 - 7)
+console.log(3 * 4)
+console.log(10 / 4)
+console.log(7 % 5)
+
+// 할당 연산자(assignment operator)
+const a = 2     //const는 재할당이 불가능함
+let b = 2       
+b += 1
+console.log(a)
+console.log(b)
